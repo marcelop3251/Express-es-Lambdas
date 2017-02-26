144bd3c3d2df1fa1c2e2f0a8005bfbfaaaa473fe
marcelo <marcelop3251@gmail.com>
2017-02-26 12:13:44 -0300

    Artigo Expressões lambda

---
+++ src/expressoes/Lambda.java
@@ -0,0 +1,34 @@
+package expressoes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import modelo.Pessoa;
+
+public class Lambda {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		  List<Pessoa> pessoas = new ArrayList<>();
+	        
+	        pessoas.add(new Pessoa("Marcelo", 28, "Marechal Rondom"));
+	        pessoas.add(new Pessoa("Tiago", 32, "Motoki koto"));
+	        pessoas.add(new Pessoa("Rubens", 40, "José de alencar"));
+	        pessoas.add(new Pessoa("Esladiane", 30, "Carlos Bombonato"));
+	        pessoas.add(new Pessoa("Stella", 5, "Jose Bonini"));
+	        pessoas.add(new Pessoa("Cleiton", 20, "Julio Marcari"));
+	        pessoas.add(new Pessoa("Luiz", 358, "José Bonifacio"));
+	      
+			new Thread(() ->  {
+				Collections.sort(pessoas, (p1,p2) -> p1.getNome().compareTo(p2.getNome()));
+				pessoas.forEach(e -> System.out.println(e.getNome()));
+				}).start();
+		
+				
+	
+       
+	}
+}
